55c1fd8f8b3eaaa644f9fe8a699eefa712f63f5c
Kacper Piętka <kacper.pietka23@wp.pl>
2025-06-19 19:52:29 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+img = cv.imread("cell.jpg")
+
+cv.imshow("Display window", img)
+
+k = cv.waitKey(0)
+
+
+#zooms in the image
+def zooming_in(img):
+    pass
+
+def zooming_out(img):
+    pass
+
+
