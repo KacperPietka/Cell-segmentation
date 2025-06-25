c2525df3f84e29d63b0471db0170b5d02721de42
Kacper Piętka <kacper.pietka23@wp.pl>
2025-06-25 23:17:02 +0200

    divided into couple files

---
+++ Image.py
@@ -0,0 +1,91 @@
+from Variables import Variables
+import cv2 as cv
+class Image:
+    def __init__(self, image_path):
+        img = cv.imread(image_path)
+        self.image = cv.resize(img, Variables.IMAGE_SIZE)
+        self.original_image = self.image.copy()
+        self.current_zoom = (0, 0, self.image.shape[1], self.image.shape[0])
+        self.zoom_factor = Variables.ORIGINAL_ZOOM_FACTOR
+        self.brightness = Variables.BRIGHTNESS_STEP
+        self.brightness_level = Variables.DEFAULT_BRIGHTNESS_LEVEL
+        self.zoom_position = Variables.DEFAULT_ZOOM_POSITION
+
+    def zoom_in(self):
+        self.zoom_factor = Variables.ORIGINAL_ZOOM_FACTOR
+        zooming_threshold = self.zoom_factor*Variables.ZOOM_THRESHOLD
+
+        x0,y0,w0,h0 = self.current_zoom
+        while self.zoom_factor < zooming_threshold:
+            self.zoom_factor += Variables.ZOOM_STEP
+            h, w = self.image.shape[:2]
+
+            new_w = int(w0 / self.zoom_factor)
+            new_h = int(h0 / self.zoom_factor)
+
+            if self.zoom_position == 'center':
+                x1 = x0 + (w0 - new_w) // 2
+                y1 = y0 + (h0 - new_h) // 2
+            elif self.zoom_position == 'top_left':
+                x1 = x0
+                y1 = y0
+            elif self.zoom_position == 'top_right':
+                x1 = x0 + w0 - new_w
+                y1 = y0
+            elif self.zoom_position == 'bottom_left':
+                x1 = x0
+                y1 = y0 + h0 - new_h
+            elif self.zoom_position == 'bottom_right':
+                x1 = x0 + w0 - new_w
+                y1 = y0 + h0 - new_h
+
+            x2 = x1 + new_w
+            y2 = y1 + new_h
+            cropped = self.image[y1:y2, x1:x2]
+            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
+            cv.imshow("Display window", zoomed_image)
+            cv.waitKey(30)
+        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
+
+
+    def zoom_out(self):
+        x0,y0,w0,h0 = self.current_zoom
+
+        h, w = self.image.shape[:2]
+        
+        self.zoom_factor = w / w0
+        center_x = x0 + w0 // 2
+        center_y = y0 + h0 // 2
+
+        if self.zoom_factor == 1:
+            return
+
+        while self.zoom_factor > Variables.ORIGINAL_ZOOM_FACTOR:
+            self.zoom_factor -= Variables.ZOOM_STEP
+            if self.zoom_factor < Variables.ORIGINAL_ZOOM_FACTOR:
+                self.zoom_factor = Variables.ORIGINAL_ZOOM_FACTOR
+            new_w = int(w / self.zoom_factor)
+            new_h = int(h / self.zoom_factor)
+
+            x1 = center_x - new_w // 2
+            y1 = center_y - new_h // 2
+
+            x1 = max(0, min(w - new_w, x1))
+            y1 = max(0, min(h - new_h, y1))
+            x2 = x1 + new_w
+            y2 = y1 + new_h
+
+            cropped = self.image[y1:y2, x1:x2]
+            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
+            cv.imshow("Display window", zoomed_image)
+            cv.waitKey(30)
+        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
+
+    def lighting_modification(self):
+        temp_img = self.original_image.copy()
+        
+        self.brightness_level += self.brightness
+
+        self.image = cv.convertScaleAbs(temp_img, beta=self.brightness_level, alpha=1.0)
+
+        cv.imshow("Display window", self.image)
--- model.py
+++ model.py
@@ -9,103 +9,8 @@ import cv2 as cv
 import numpy as np
 from cellpose import models
 from cellpose.io import imread
-
-class Config:
-    IMAGE_SIZE = (1200, 1000)
-    BRIGHTNESS_STEP = 50
-    CELLPOSE_DIAMETER = 30
-    ORIGINAL_ZOOM_FACTOR = 1
-    DEFAULT_ZOOM_POSITION = 'center'
-    DEFAULT_BRIGHTNESS_LEVEL = 0
-
-class Image:
-    def __init__(self, image_path):
-        img = cv.imread(image_path)
-        self.image = cv.resize(img, (1200, 1000))
-        self.original_image = self.image.copy()
-        self.current_zoom = (0, 0, self.image.shape[1], self.image.shape[0])
-        self.zoom_factor = 1
-        self.brightness = 50
-        self.brightness_level = 0
-        self.zoom_position = 'center'
-
-    def zoom_in(self):
-        self.zoom_factor = 1.0
-        zooming_threshold = self.zoom_factor*1.5
-
-        x0,y0,w0,h0 = self.current_zoom
-        while self.zoom_factor < zooming_threshold:
-            self.zoom_factor += 0.05
-            h, w = self.image.shape[:2]
-
-            new_w = int(w0 / self.zoom_factor)
-            new_h = int(h0 / self.zoom_factor)
-
-            if self.zoom_position == 'center':
-                x1 = x0 + (w0 - new_w) // 2
-                y1 = y0 + (h0 - new_h) // 2
-            elif self.zoom_position == 'top_left':
-                x1 = x0
-                y1 = y0
-            elif self.zoom_position == 'top_right':
-                x1 = x0 + w0 - new_w
-                y1 = y0
-            elif self.zoom_position == 'bottom_left':
-                x1 = x0
-                y1 = y0 + h0 - new_h
-            elif self.zoom_position == 'bottom_right':
-                x1 = x0 + w0 - new_w
-                y1 = y0 + h0 - new_h
-
-            x2 = x1 + new_w
-            y2 = y1 + new_h
-            cropped = self.image[y1:y2, x1:x2]
-            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
-            cv.imshow("Display window", zoomed_image)
-            cv.waitKey(30)
-        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
-
-
-    def zoom_out(self):
-        x0,y0,w0,h0 = self.current_zoom
-        h, w = self.image.shape[:2]
-        
-        self.zoom_factor = w / w0
-        center_x = x0 + w0 // 2
-        center_y = y0 + h0 // 2
-
-        if self.zoom_factor == 1:
-            return
-
-        while self.zoom_factor > 1:
-            self.zoom_factor -= 0.05
-            if self.zoom_factor < 1:
-                self.zoom_factor = 1
-            new_w = int(w / self.zoom_factor)
-            new_h = int(h / self.zoom_factor)
-
-            x1 = center_x - new_w // 2
-            y1 = center_y - new_h // 2
-
-            x1 = max(0, min(w - new_w, x1))
-            y1 = max(0, min(h - new_h, y1))
-            x2 = x1 + new_w
-            y2 = y1 + new_h
-
-            cropped = self.image[y1:y2, x1:x2]
-            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
-            cv.imshow("Display window", zoomed_image)
-            cv.waitKey(30)
-        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
-
-    def lighting_modification(self):
-        temp_img = self.original_image.copy()
-        
-        self.brightness_level += self.brightness
-
-        self.image = cv.convertScaleAbs(temp_img, beta=self.brightness_level, alpha=1.0)
-
-        cv.imshow("Display window", self.image)
+from Variables import Variables
+from Image import Image
 
 class Model(Image):
     def __init__(self, image_path):
@@ -114,7 +19,7 @@ class Model(Image):
         self.speech = sr.Recognizer()
         self.response_text = ""
         self.command = None
-        self.color = [0, 0, 255]
+        self.color = Variables.RED_BGR
         #self.move_position = None
         self.user_input = ""
         self.end = False
@@ -271,7 +176,7 @@ class Model(Image):
         self.image = self.original_image.copy()
 
         model = models.CellposeModel(gpu=True)
-        masks, flows, styles = model.eval(self.image, diameter=30)
+        masks, flows, styles = model.eval(self.image, diameter=Variables.CELLPOSE_DIAMETER)
 
         image_BGR = self.image.copy()
 
@@ -279,13 +184,11 @@ class Model(Image):
 
         overlay_color = tuple(int(c) for c in self.color)
 
-        alpha = 0.5
-
         # For each channel, blend overlay_color onto image where mask is 1 (Chatgpt)
         for c in range(3):
             image_BGR[:, :, c] = np.where(
                 binary_mask == 1,
-                (alpha * overlay_color[c] + (1 - alpha) * image_BGR[:, :, c]).astype(np.uint8),
+                (Variables.ALPHA * overlay_color[c] + (1 - Variables.ALPHA) * image_BGR[:, :, c]).astype(np.uint8),
                 image_BGR[:, :, c]
             )
 
