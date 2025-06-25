35195c908ec75a0178345b6a3fa16a66d1d7efb9
Kacper Piętka <kacper.pietka23@wp.pl>
2025-06-25 22:01:44 +0200

    changes to code

--- model.py
+++ model.py
@@ -10,23 +10,112 @@ import numpy as np
 from cellpose import models
 from cellpose.io import imread
 
+class Config:
+    IMAGE_SIZE = (1200, 1000)
+    BRIGHTNESS_STEP = 50
+    CELLPOSE_DIAMETER = 30
+    ORIGINAL_ZOOM_FACTOR = 1
+    DEFAULT_ZOOM_POSITION = 'center'
+    DEFAULT_BRIGHTNESS_LEVEL = 0
 
-class Model:
+class Image:
     def __init__(self, image_path):
-        self.url = 'http://localhost:11434/api/chat'
         img = cv.imread(image_path)
         self.image = cv.resize(img, (1200, 1000))
         self.original_image = self.image.copy()
-        self.speech = sr.Recognizer()
-        self.response_text = ""
         self.current_zoom = (0, 0, self.image.shape[1], self.image.shape[0])
         self.zoom_factor = 1
         self.brightness = 50
         self.brightness_level = 0
-        self.command = None
-        self.color = [0, 0, 255] #red initially
-        self.move_position = None
         self.zoom_position = 'center'
+
+    def zoom_in(self):
+        self.zoom_factor = 1.0
+        zooming_threshold = self.zoom_factor*1.5
+
+        x0,y0,w0,h0 = self.current_zoom
+        while self.zoom_factor < zooming_threshold:
+            self.zoom_factor += 0.05
+            h, w = self.image.shape[:2]
+
+            new_w = int(w0 / self.zoom_factor)
+            new_h = int(h0 / self.zoom_factor)
+
+            if self.zoom_position == 'center':
+                x1 = x0 + (w0 - new_w) // 2
+                y1 = y0 + (h0 - new_h) // 2
+            elif self.zoom_position == 'top_left':
+                x1 = x0
+                y1 = y0
+            elif self.zoom_position == 'top_right':
+                x1 = x0 + w0 - new_w
+                y1 = y0
+            elif self.zoom_position == 'bottom_left':
+                x1 = x0
+                y1 = y0 + h0 - new_h
+            elif self.zoom_position == 'bottom_right':
+                x1 = x0 + w0 - new_w
+                y1 = y0 + h0 - new_h
+
+            x2 = x1 + new_w
+            y2 = y1 + new_h
+            cropped = self.image[y1:y2, x1:x2]
+            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
+            cv.imshow("Display window", zoomed_image)
+            cv.waitKey(30)
+        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
+
+
+    def zoom_out(self):
+        x0,y0,w0,h0 = self.current_zoom
+        h, w = self.image.shape[:2]
+        
+        self.zoom_factor = w / w0
+        center_x = x0 + w0 // 2
+        center_y = y0 + h0 // 2
+
+        if self.zoom_factor == 1:
+            return
+
+        while self.zoom_factor > 1:
+            self.zoom_factor -= 0.05
+            if self.zoom_factor < 1:
+                self.zoom_factor = 1
+            new_w = int(w / self.zoom_factor)
+            new_h = int(h / self.zoom_factor)
+
+            x1 = center_x - new_w // 2
+            y1 = center_y - new_h // 2
+
+            x1 = max(0, min(w - new_w, x1))
+            y1 = max(0, min(h - new_h, y1))
+            x2 = x1 + new_w
+            y2 = y1 + new_h
+
+            cropped = self.image[y1:y2, x1:x2]
+            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
+            cv.imshow("Display window", zoomed_image)
+            cv.waitKey(30)
+        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
+
+    def lighting_modification(self):
+        temp_img = self.original_image.copy()
+        
+        self.brightness_level += self.brightness
+
+        self.image = cv.convertScaleAbs(temp_img, beta=self.brightness_level, alpha=1.0)
+
+        cv.imshow("Display window", self.image)
+
+class Model(Image):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+        self.url = 'http://localhost:11434/api/chat'
+        self.speech = sr.Recognizer()
+        self.response_text = ""
+        self.command = None
+        self.color = [0, 0, 255]
+        #self.move_position = None
         self.user_input = ""
         self.end = False
         self.payload = {
@@ -175,74 +264,6 @@ class Model:
         else:
             print("Error")
 
-    def zoom_in(self):
-        self.zoom_factor = 1.0
-        zooming_threshold = self.zoom_factor*1.5
-
-        x0,y0,w0,h0 = self.current_zoom
-        while self.zoom_factor < zooming_threshold:
-            self.zoom_factor += 0.05
-            h, w = self.image.shape[:2]
-
-            new_w = int(w0 / self.zoom_factor)
-            new_h = int(h0 / self.zoom_factor)
-
-            if self.zoom_position == 'center':
-                x1 = x0 + (w0 - new_w) // 2
-                y1 = y0 + (h0 - new_h) // 2
-            elif self.zoom_position == 'top_left':
-                x1 = x0
-                y1 = y0
-            elif self.zoom_position == 'top_right':
-                x1 = x0 + w0 - new_w
-                y1 = y0
-            elif self.zoom_position == 'bottom_left':
-                x1 = x0
-                y1 = y0 + h0 - new_h
-            elif self.zoom_position == 'bottom_right':
-                x1 = x0 + w0 - new_w
-                y1 = y0 + h0 - new_h
-
-            x2 = x1 + new_w
-            y2 = y1 + new_h
-            cropped = self.image[y1:y2, x1:x2]
-            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
-            cv.imshow("Display window", zoomed_image)
-            cv.waitKey(30)
-        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
-
-
-    def zoom_out(self):
-        x0,y0,w0,h0 = self.current_zoom
-        h, w = self.image.shape[:2]
-        
-        self.zoom_factor = w / w0
-        center_x = x0 + w0 // 2
-        center_y = y0 + h0 // 2
-
-        if self.zoom_factor == 1:
-            return
-
-        while self.zoom_factor > 1:
-            self.zoom_factor -= 0.05
-            if self.zoom_factor < 1:
-                self.zoom_factor = 1
-            new_w = int(w / self.zoom_factor)
-            new_h = int(h / self.zoom_factor)
-
-            x1 = center_x - new_w // 2
-            y1 = center_y - new_h // 2
-
-            x1 = max(0, min(w - new_w, x1))
-            y1 = max(0, min(h - new_h, y1))
-            x2 = x1 + new_w
-            y2 = y1 + new_h
-
-            cropped = self.image[y1:y2, x1:x2]
-            zoomed_image = cv.resize(cropped, (w, h), interpolation=cv.INTER_LINEAR)
-            cv.imshow("Display window", zoomed_image)
-            cv.waitKey(30)
-        self.current_zoom = (x1, y1, x2 - x1, y2 - y1)
 
 
     def cell_segmentation(self):
@@ -270,15 +291,6 @@ class Model:
 
         self.image = image_BGR
         self.image = cv.convertScaleAbs(self.image, beta=self.brightness_level, alpha=1.0)
-        cv.imshow("Display window", self.image)
-
-    def lighting_modification(self):
-        temp_img = self.original_image.copy()
-        
-        self.brightness_level += self.brightness
-
-        self.image = cv.convertScaleAbs(temp_img, beta=self.brightness_level, alpha=1.0)
-
         cv.imshow("Display window", self.image)
 
     def quit(self):
@@ -329,8 +341,11 @@ class Model:
         cv.waitKey(30)
         self.current_zoom = (x1, y1, w0, h0)
     """
+
+
+image = Image("./images/image1.png")
 model = Model("./images/image1.png")
-cv.imshow("Display window", model.image)
+cv.imshow("Display window", image.image)
 cv.waitKey(1)
 
 while model.end != True:
